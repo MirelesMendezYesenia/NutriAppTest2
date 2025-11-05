492d9cc8bc0deb5b58fdc30642cd1163a62e0123
MirelesMendezYesenia <23308060610600@cetis61.edu.mx>
2025-11-04 17:40:10 -0700

    actualizacion

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/recetas")
+def recetas():
+    return render_template("recetas.html")
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
+
 if __name__ == '__main__':
     app.run(debug=True)
